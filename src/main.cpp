08a622cc60730a10dc2aa2a4646e6661bb04f44e
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2025-05-05 23:30:53 +0200

    Create and show Win32 window

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,89 @@
 #include <windows.h>
+#include <stdio.h>
 
-int CALLBACK WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLIne,
-	int nCmdShow
+LRESULT CALLBACK on_window_event(
+	HWND window,
+	UINT message,
+	WPARAM w_param,
+	LPARAM l_param
 ) {
-	MessageBoxA(0, "This is Handmade Hero!", "Handmade Hero", MB_OK | MB_ICONINFORMATION);
+	LRESULT result = 0;
+
+	switch (message) {
+		case WM_SIZE: {
+			/* handle resized */
+			printf("WM_SIZE\n");
+		} break;
+
+		case WM_DESTROY: {
+			printf("WM_DESTROY\n");
+			PostQuitMessage(0);
+			return 0;
+		} break;
+
+		case WM_CLOSE: {
+			printf("WM_CLOSE\n");
+			PostQuitMessage(0);
+			return 0;
+		} break;
+
+		case WM_ACTIVATEAPP: {
+			/* app has been actived */
+		} break;
+	}
+
+	return DefWindowProc(window, message, w_param, l_param);
+}
+
+int WINAPI WinMain(
+	HINSTANCE instance,
+	HINSTANCE /*prev_instance*/,
+	LPSTR /*command_line*/,
+	int /*command_show*/
+) {
+	/* Register window class */
+	WNDCLASSW window_class = {
+		.style =
+			CS_OWNDC // give this window a unique device context
+			| CS_HREDRAW | CS_VREDRAW, // redraw window when resized
+		.lpfnWndProc = on_window_event,
+		.hInstance = instance,
+		.lpszClassName = L"HandmadeHeroWindowClass",
+	};
+	bool register_result = RegisterClassW(&window_class);
+	if (!register_result) {
+		// TODO handle and abort
+	}
+
+	/* Create window */
+	HWND window_handle = CreateWindowExW(
+		0, // DWORD dwExStyle
+		window_class.lpszClassName, // LPCWSTR lpClassName
+		L"Handmade Hero", // LPCWSTR lpWindowName
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE, // DWORD dwStyle
+		CW_USEDEFAULT, // int X
+		CW_USEDEFAULT, // int Y
+		CW_USEDEFAULT, // int nWidth
+		CW_USEDEFAULT, // int nHeight
+		0, // HWND hWndParent
+		0, // HMENU hMenu
+		instance, // HINSTANCE hInstance
+		0 // LPVOID lpParam
+	);
+	if (!window_handle) {
+		// TODO handle and abort
+	}
+
+	/* Handle messages */
+	while (true) {
+		MSG message;
+		if (GetMessageW(&message, 0, 0, 0) <= 0) {
+			break;
+		}
+
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+	}
+
+	return 0;
 }
