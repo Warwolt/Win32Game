19f61e47655ac56a3aa35c2b7b84db6d9bb9aa7b
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2025-05-13 22:33:06 +0200

    Keyboard test cases

--- test/engine/keyboard_tests.cpp
+++ test/engine/keyboard_tests.cpp
@@ -6,7 +6,7 @@ using namespace engine;
 
 constexpr uint32_t TEST_KEY_ID = 1234;
 
-TEST(KeyboardTests, KeysAreInitiallyReleased) {
+TEST(KeyboardTests, InitiallyReleased) {
 	Keyboard keyboard;
 
     EXPECT_TRUE(keyboard.key_is_released(TEST_KEY_ID));
@@ -14,3 +14,87 @@ TEST(KeyboardTests, KeysAreInitiallyReleased) {
     EXPECT_FALSE(keyboard.key_was_released_now(TEST_KEY_ID));
 	EXPECT_FALSE(keyboard.key_was_pressed_now(TEST_KEY_ID));
 }
+
+TEST(KeyboardTests, Pressed) {
+	Keyboard keyboard;
+
+    // push
+    keyboard.on_key_event(TEST_KEY_ID, true);
+    keyboard.update();
+
+	EXPECT_FALSE(keyboard.key_is_released(TEST_KEY_ID));
+	EXPECT_TRUE(keyboard.key_is_pressed(TEST_KEY_ID));
+    EXPECT_FALSE(keyboard.key_was_released_now(TEST_KEY_ID));
+	EXPECT_TRUE(keyboard.key_was_pressed_now(TEST_KEY_ID));
+}
+
+TEST(KeyboardTests, Pressed_WhileIgnoringEvents) {
+	Keyboard keyboard;
+
+    // push
+    keyboard.on_key_event(TEST_KEY_ID, true); // should be ignored
+    keyboard.on_key_event(TEST_KEY_ID, true);
+    keyboard.update();
+
+	EXPECT_FALSE(keyboard.key_is_released(TEST_KEY_ID));
+	EXPECT_TRUE(keyboard.key_is_pressed(TEST_KEY_ID));
+    EXPECT_FALSE(keyboard.key_was_released_now(TEST_KEY_ID));
+	EXPECT_TRUE(keyboard.key_was_pressed_now(TEST_KEY_ID));
+}
+
+TEST(KeyboardTests, HeldDown) {
+	Keyboard keyboard;
+
+    // push
+    keyboard.on_key_event(TEST_KEY_ID, true);
+    keyboard.update();
+
+    // keep held down
+    keyboard.on_key_event(TEST_KEY_ID, true);
+    keyboard.update();
+
+	EXPECT_FALSE(keyboard.key_is_released(TEST_KEY_ID));
+	EXPECT_TRUE(keyboard.key_is_pressed(TEST_KEY_ID));
+    EXPECT_FALSE(keyboard.key_was_released_now(TEST_KEY_ID));
+	EXPECT_FALSE(keyboard.key_was_pressed_now(TEST_KEY_ID));
+}
+
+TEST(KeyboardTests, Released) {
+	Keyboard keyboard;
+
+    // push
+    keyboard.on_key_event(TEST_KEY_ID, true);
+    keyboard.update();
+
+    // release
+    keyboard.on_key_event(TEST_KEY_ID, false);
+    keyboard.update();
+
+	EXPECT_TRUE(keyboard.key_is_released(TEST_KEY_ID));
+	EXPECT_FALSE(keyboard.key_is_pressed(TEST_KEY_ID));
+	EXPECT_TRUE(keyboard.key_was_released_now(TEST_KEY_ID));
+	EXPECT_FALSE(keyboard.key_was_pressed_now(TEST_KEY_ID));
+}
+
+
+TEST(KeyboardTests, HeldUp) {
+	Keyboard keyboard;
+
+    // push
+    keyboard.on_key_event(TEST_KEY_ID, true);
+    keyboard.update();
+
+    // release
+    keyboard.on_key_event(TEST_KEY_ID, false);
+    keyboard.update();
+
+    // hold up
+    keyboard.on_key_event(TEST_KEY_ID, false);
+    keyboard.update();
+
+	EXPECT_TRUE(keyboard.key_is_released(TEST_KEY_ID));
+	EXPECT_FALSE(keyboard.key_is_pressed(TEST_KEY_ID));
+	EXPECT_FALSE(keyboard.key_was_released_now(TEST_KEY_ID));
+	EXPECT_FALSE(keyboard.key_was_pressed_now(TEST_KEY_ID));
+}
+
