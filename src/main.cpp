a08ea9b3b931e4a2a964e32681e5bd6f26f3729f
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2025-05-14 20:56:13 +0200

    Start sketching AudioPlayer class

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,50 @@
 #include <winrt/base.h>
 #include <xaudio2.h>
 
+namespace engine {
+
+	class AudioPlayer {
+	public:
+		AudioPlayer() = default;
+		static AudioPlayer initialize_audio_player();
+
+	private:
+		winrt::com_ptr<IXAudio2> m_xaudio2_engine = {};
+		IXAudio2MasteringVoice* m_xaudio2_mastering_voice = {};
+		IXAudio2SourceVoice* m_xaudio2_source_voice = {};
+		XAUDIO2_BUFFER m_x_audio2_buffer = {};
+
+	};
+
+	AudioPlayer AudioPlayer::initialize_audio_player() {
+		AudioPlayer audio_player;
+
+		/* Initialize audio engine */
+		winrt::check_hresult(XAudio2Create(audio_player.m_xaudio2_engine.put(), 0, XAUDIO2_DEFAULT_PROCESSOR));
+		winrt::check_hresult(audio_player.m_xaudio2_engine->CreateMasteringVoice(&audio_player.m_xaudio2_mastering_voice));
+
+		/* Add voices */
+		constexpr WORD NUMBER_OF_CHANNELS = 1;
+		constexpr WORD BITS_PER_SAMPLE = 16;
+		constexpr WORD SAMPLES_PER_SEC = 44100;
+		constexpr WORD BITS_PER_BYTE = 8;
+		constexpr WORD BLOCK_ALIGNMENT = NUMBER_OF_CHANNELS * BITS_PER_SAMPLE / BITS_PER_BYTE;
+		WAVEFORMATEX wav_format = {
+			.wFormatTag = WAVE_FORMAT_PCM,
+			.nChannels = NUMBER_OF_CHANNELS, // mono or stero
+			.nSamplesPerSec = SAMPLES_PER_SEC,
+			.nAvgBytesPerSec = SAMPLES_PER_SEC * BLOCK_ALIGNMENT,
+			.nBlockAlign = BLOCK_ALIGNMENT,
+			.wBitsPerSample = BITS_PER_SAMPLE,
+		};
+
+		winrt::check_hresult(audio_player.m_xaudio2_engine->CreateSourceVoice(&audio_player.m_xaudio2_source_voice, &wav_format));
+
+		return audio_player;
+	}
+
+} // namespace engine
+
 // from https://learn.microsoft.com/en-us/windows/win32/xaudio2/how-to--load-audio-data-files-in-xaudio2
 constexpr int fourccRIFF = 'FFIR';
 constexpr int fourccDATA = 'atad';
@@ -18,7 +62,7 @@ constexpr int fourccFMT = ' tmf';
 constexpr int fourccWAVE = 'EVAW';
 constexpr int fourccXWMA = 'AMWX';
 constexpr int fourccDPDS = 'sdpd';
-HRESULT FindChunk(HANDLE hFile, DWORD fourcc, DWORD& dwChunkSize, DWORD& dwChunkDataPosition) {
+static HRESULT find_chunk_in_file(HANDLE hFile, DWORD fourcc, DWORD* dwChunkSize, DWORD* dwChunkDataPosition) {
 	HRESULT hr = S_OK;
 	if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, 0, NULL, FILE_BEGIN))
 		return HRESULT_FROM_WIN32(GetLastError());
@@ -54,8 +98,8 @@ HRESULT FindChunk(HANDLE hFile, DWORD fourcc, DWORD& dwChunkSize, DWORD& dwChunk
 		dwOffset += sizeof(DWORD) * 2;
 
 		if (dwChunkType == fourcc) {
-			dwChunkSize = dwChunkDataSize;
-			dwChunkDataPosition = dwOffset;
+			*dwChunkSize = dwChunkDataSize;
+			*dwChunkDataPosition = dwOffset;
 			return S_OK;
 		}
 
@@ -68,7 +112,7 @@ HRESULT FindChunk(HANDLE hFile, DWORD fourcc, DWORD& dwChunkSize, DWORD& dwChunk
 }
 
 // from https://learn.microsoft.com/en-us/windows/win32/xaudio2/how-to--load-audio-data-files-in-xaudio2
-HRESULT ReadChunkData(HANDLE hFile, void* buffer, DWORD buffersize, DWORD bufferoffset) {
+static HRESULT read_chunk_from_file(HANDLE hFile, void* buffer, DWORD buffersize, DWORD bufferoffset) {
 	HRESULT hr = S_OK;
 	if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, bufferoffset, NULL, FILE_BEGIN))
 		return HRESULT_FROM_WIN32(GetLastError());
@@ -175,30 +219,29 @@ int WINAPI WinMain(
 	{
 		// code based on https://learn.microsoft.com/en-us/windows/win32/xaudio2/full-project
 
-		constexpr WORD BITS_PERS_SAMPLE = 16;
-		constexpr DWORD SAMPLES_PER_SEC = 44100;
-		constexpr double CYCLES_PER_SEC = 220.0;
-		constexpr WORD AUDIO_BUFFER_SIZE_IN_CYCLES = 10;
-		constexpr DWORD SAMPLES_PER_CYCLE = (DWORD)(SAMPLES_PER_SEC / CYCLES_PER_SEC);                  // 200 samples per cycle.
-		constexpr DWORD AUDIO_BUFFER_SIZE_IN_SAMPLES = SAMPLES_PER_CYCLE * AUDIO_BUFFER_SIZE_IN_CYCLES; // 2,000 samples per buffer.
-
 		// Initialize engine and master voice
 		winrt::check_hresult(XAudio2Create(xaudio2_engine.put(), 0, XAUDIO2_DEFAULT_PROCESSOR));
 		winrt::check_hresult(xaudio2_engine->CreateMasteringVoice(&xaudio2_mastering_voice));
 
-		// Define a format
-		WAVEFORMATEX wave_format_ex = {};
-		wave_format_ex.wFormatTag = WAVE_FORMAT_PCM;
-		wave_format_ex.nChannels = 1; // 1 channel
-		wave_format_ex.nSamplesPerSec = SAMPLES_PER_SEC;
-		wave_format_ex.nBlockAlign = wave_format_ex.nChannels * BITS_PERS_SAMPLE / 8;
-		wave_format_ex.nAvgBytesPerSec = wave_format_ex.nSamplesPerSec * wave_format_ex.nBlockAlign;
-		wave_format_ex.wBitsPerSample = BITS_PERS_SAMPLE;
-		wave_format_ex.cbSize = 0;
+		constexpr WORD NUMBER_OF_CHANNELS = 1;
+		constexpr WORD BITS_PER_SAMPLE = 16;
+		constexpr WORD SAMPLES_PER_SEC = 44100;
+		constexpr WORD BITS_PER_BYTE = 8;
+		constexpr WORD BLOCK_ALIGNMENT = NUMBER_OF_CHANNELS * BITS_PER_SAMPLE / BITS_PER_BYTE;
 
-		// Create source voice using format and start it
+		// Define a format
+		WAVEFORMATEX wave_format_ex = {
+			.wFormatTag = WAVE_FORMAT_PCM,
+			.nChannels = NUMBER_OF_CHANNELS, // mono or stero
+			.nSamplesPerSec = SAMPLES_PER_SEC,
+			.nAvgBytesPerSec = SAMPLES_PER_SEC * BLOCK_ALIGNMENT,
+			.nBlockAlign = BLOCK_ALIGNMENT,
+			.wBitsPerSample = BITS_PER_SAMPLE,
+			.cbSize = 0,
+		};
+
+		// Create source voice using format
 		winrt::check_hresult(xaudio2_engine->CreateSourceVoice(&xaudio2_source_voice, &wave_format_ex));
-		xaudio2_source_voice->Start(0);
 
 		// Load wave file
 		HANDLE cowbell_file = CreateFileA(
@@ -219,23 +262,23 @@ int WINAPI WinMain(
 		DWORD chunk_size;
 		DWORD chunk_position;
 		//check the file type, should be fourccWAVE or 'XWMA'
-		FindChunk(cowbell_file, fourccRIFF, chunk_size, chunk_position);
+		find_chunk_in_file(cowbell_file, fourccRIFF, &chunk_size, &chunk_position);
 		DWORD filetype;
-		ReadChunkData(cowbell_file, &filetype, sizeof(DWORD), chunk_position);
+		read_chunk_from_file(cowbell_file, &filetype, sizeof(DWORD), chunk_position);
 		if (filetype != fourccWAVE) {
 			fprintf(stderr, "File wasn't right format");
 			exit(1);
 		}
 		WAVEFORMATEX cowbell_format = {};
-		FindChunk(cowbell_file, fourccFMT, chunk_size, chunk_position);
-		ReadChunkData(cowbell_file, &cowbell_format, chunk_size, chunk_position);
+		find_chunk_in_file(cowbell_file, fourccFMT, &chunk_size, &chunk_position);
+		read_chunk_from_file(cowbell_file, &cowbell_format, chunk_size, chunk_position);
 		//fill out the audio data buffer with the contents of the fourccDATA chunk
-		FindChunk(cowbell_file, fourccDATA, chunk_size, chunk_position);
+		find_chunk_in_file(cowbell_file, fourccDATA, &chunk_size, &chunk_position);
 		BYTE* cowbell_buffer = new BYTE[chunk_size];
-		ReadChunkData(cowbell_file, cowbell_buffer, chunk_size, chunk_position);
+		read_chunk_from_file(cowbell_file, cowbell_buffer, chunk_size, chunk_position);
 
-		x_audio2_buffer.AudioBytes = chunk_size;       //size of the audio buffer in bytes
-		x_audio2_buffer.pAudioData = cowbell_buffer;   //buffer containing audio data
+		x_audio2_buffer.AudioBytes = chunk_size;       // size of the audio buffer in bytes
+		x_audio2_buffer.pAudioData = cowbell_buffer;   // buffer containing audio data
 		x_audio2_buffer.Flags = XAUDIO2_END_OF_STREAM; // tell the source voice not to expect any data after this buffer
 	}
 
