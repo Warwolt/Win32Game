7acc5b2881aff01748b778ac24156eee7fc52990
Rasmus Källqvist <rkallqvist@microsoft.com>
2025-10-14 23:29:25 +0200

    Move comment

--- src/library/application_library.cpp
+++ src/library/application_library.cpp
@@ -48,9 +48,8 @@ State* initialize_application(HINSTANCE instance, WNDPROC on_window_event) {
 }
 
 LRESULT CALLBACK on_window_event(State* state, HWND window, UINT message, WPARAM w_param, LPARAM l_param) {
-	/* Check hot reload key */
+	/* Trigger hot reload */
 	if (message == WM_KEYDOWN && w_param == VK_F5) {
-		/* Trigger hot reload */
 		std::expected<void, std::string> reload_start_result = g_loader.trigger_hot_reload();
 		if (!reload_start_result) {
 			fprintf(stderr, "Error when triggering library hot reload: %s\n", reload_start_result.error().c_str());
