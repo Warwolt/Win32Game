4b88047b2b6d689d426b6862499acd7f94de6136
Rasmus Källqvist <rkallqvist@microsoft.com>
2025-10-19 21:19:25 +0200

    remove old class def

--- src/engine/debug/test_screens/test_screens.h
+++ src/engine/debug/test_screens/test_screens.h
@@ -6,6 +6,7 @@
 #include <engine/input/time.h>
 #include <engine/math/ivec2.h>
 #include <engine/debug/test_screens/geometry_test_screen.h>
+#include <engine/debug/test_screens/image_test_screen.h>
 
 #include <vector>
 
@@ -22,28 +23,6 @@ namespace engine {
 			ImageTest,
 			Count,
 		};
-	};
-
-	class ImageTestScreen {
-	public:
-		void initialize(ResourceManager* resources);
-		void update(const InputDevices& input);
-		void draw(Renderer* renderer, IVec2 screen_resolution) const;
-
-	private:
-		struct AnimationFrame {
-			int index;
-			bool flipped;
-		};
-
-		ImageID m_clipping_image;
-		ImageID m_transparency_image;
-
-		ImageID m_sprite_sheet;
-		int m_animation_index = 0;
-		Rect m_sprite_sheet_size;
-		std::vector<AnimationFrame> m_animation_frames;
-		Time m_last_sprite_sheet_frame;
 	};
 
 	class TestScreens {
