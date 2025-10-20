5d25369bef6335ad67cc4577fa8ffbca01f07050
Rasmus Källqvist <rkallqvist@microsoft.com>
2025-10-20 23:29:00 +0200

    Add milliseconds and seconds constructor to Time

--- src/engine/input/time.h
+++ src/engine/input/time.h
@@ -13,6 +13,8 @@ namespace engine {
 		static Time now(); // since epoch
 		Time() = default;
 		Time(std::chrono::nanoseconds ns) : value(ns) {}
+		Time(std::chrono::milliseconds ms) : value(std::chrono::duration_cast<std::chrono::nanoseconds>(ms)) {}
+		Time(std::chrono::seconds ms) : value(std::chrono::duration_cast<std::chrono::nanoseconds>(ms)) {}
 		friend std::chrono::nanoseconds operator-(Time lhs, const Time& rhs);
 	};
 
