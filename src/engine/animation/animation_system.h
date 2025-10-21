85d8e8288c629251e9335b1387b41db1149dbf8a
Rasmus Källqvist <rkallqvist@microsoft.com>
2025-10-21 22:45:29 +0200

    start animation basics

--- src/engine/animation/animation_system.h
+++ src/engine/animation/animation_system.h
@@ -3,18 +3,19 @@
 #include <engine/input/time.h>
 
 #include <expected>
+#include <unordered_map>
 #include <vector>
 
 namespace engine {
 
 	struct AnimationID {
 		int value;
-        bool operator==(const AnimationID& rhs) const = default;
+		bool operator==(const AnimationID& rhs) const = default;
 	};
 
 	struct AnimationEntityID {
 		int value;
-        bool operator==(const AnimationEntityID& rhs) const = default;
+		bool operator==(const AnimationEntityID& rhs) const = default;
 	};
 
 	constexpr AnimationID INVALID_ANIMATION_ID = AnimationID(0);
@@ -34,23 +35,31 @@ namespace engine {
 	template <typename T>
 	class AnimationSystem {
 	public:
-		AnimationID add_animation(std::vector<AnimationFrame<T>> animation) {
-            if (animation.empty()) {
-                return INVALID_ANIMATION_ID;
-            }
+		AnimationID add_animation(std::vector<AnimationFrame<T>> animation_frames) {
+			if (animation_frames.empty()) {
+				return INVALID_ANIMATION_ID;
+			}
 
 			AnimationID id = AnimationID(m_next_id++);
-			// add animation here
+			m_animations[id.value].frames = animation_frames;
+
 			return id;
 		}
 
-		std::expected<void, AnimationError> start_animation(AnimationID animation_id, AnimationEntityID entity_id, Time /*now*/) {
-			if (animation_id == INVALID_ANIMATION_ID) {
+		std::expected<void, AnimationError> start_animation(AnimationID animation_id, AnimationEntityID entity_id, Time now) {
+			const bool missing_animation = m_animations.find(animation_id.value) == m_animations.end();
+			if (animation_id == INVALID_ANIMATION_ID || missing_animation) {
 				return std::unexpected(AnimationError::InvalidAnimationID);
 			}
+
 			if (entity_id == INVALID_ANIMATION_ENTITY_ID) {
 				return std::unexpected(AnimationError::InvalidAnimationEntityID);
 			}
+
+			m_playback[entity_id.value] = Playback {
+				.animation_id = animation_id,
+				.start = now,
+			};
 
 			return {};
 		}
@@ -58,13 +67,28 @@ namespace engine {
 		void update(Time /*now*/) {
 		}
 
-		const T& current_frame(AnimationEntityID /*entity_id*/) const {
-			return m_default;
+		const T& current_frame(AnimationEntityID entity_id) const {
+			auto it = m_playback.find(entity_id.value);
+			if (it == m_playback.end()) {
+				return m_default;
+			}
+			const Playback& playback = it->second;
+			return m_animations.at(playback.animation_id.value).frames[0].value;
 		}
 
 	private:
+		struct Animation {
+			std::vector<AnimationFrame<T>> frames;
+		};
+		struct Playback {
+			AnimationID animation_id;
+			Time start;
+		};
+
 		int m_next_id = 1;
 		T m_default = {};
+		std::unordered_map<int, Animation> m_animations; // AnimationID -> Animation
+		std::unordered_map<int, Playback> m_playback;    // AnimationEntityID -> Playback
 	};
 
 } // namespace engine
--- test/engine/animation_system_tests.cpp
+++ test/engine/animation_system_tests.cpp
@@ -6,18 +6,26 @@ using namespace engine;
 
 std::vector<AnimationFrame<int>> test_animation() {
 	return {
-		{ 1, 100ms },
-		{ 2, 100ms },
-		{ 3, 100ms },
+		{ 111, 100ms },
+		{ 222, 100ms },
+		{ 333, 100ms },
 	};
 }
 
 TEST(AnimationSystemTests, AddAnimation_NoFrames_GivesInvalidAnimationID) {
 	AnimationSystem<int> animation_system;
 
-    AnimationID animation_id = animation_system.add_animation({});
+	AnimationID animation_id = animation_system.add_animation({});
 
-    EXPECT_EQ(animation_id, INVALID_ANIMATION_ID);
+	EXPECT_EQ(animation_id, INVALID_ANIMATION_ID);
+}
+
+TEST(AnimationSystemTests, CurrentFrame_InvalidAnimationID_GivesDefaultAnimationFrame) {
+	AnimationSystem<int> animation_system;
+
+	int current_frame = animation_system.current_frame(INVALID_ANIMATION_ENTITY_ID);
+
+	EXPECT_EQ(current_frame, 0);
 }
 
 TEST(AnimationSystemTests, StartAnimation_InvalidAnimationID_GivesError) {
@@ -25,6 +33,17 @@ TEST(AnimationSystemTests, StartAnimation_InvalidAnimationID_GivesError) {
 	AnimationEntityID entity_id = AnimationEntityID(1);
 
 	std::expected<void, AnimationError> result = animation_system.start_animation(INVALID_ANIMATION_ID, entity_id, 0ms);
+
+	ASSERT_FALSE(result.has_value());
+	EXPECT_EQ(result.error(), AnimationError::InvalidAnimationID);
+}
+
+TEST(AnimationSystemTests, StartAnimation_NonRegisteredAnimationID_GivesError) {
+	AnimationSystem<int> animation_system;
+	AnimationID animation_id = AnimationID(1); // we haven't added this animation to the system
+	AnimationEntityID entity_id = AnimationEntityID(1);
+
+	std::expected<void, AnimationError> result = animation_system.start_animation(animation_id, entity_id, 0ms);
 
 	ASSERT_FALSE(result.has_value());
 	EXPECT_EQ(result.error(), AnimationError::InvalidAnimationID);
@@ -40,14 +59,16 @@ TEST(AnimationSystemTests, StartAnimation_InvalidAnimationEntityID_GivesError) {
 	EXPECT_EQ(result.error(), AnimationError::InvalidAnimationEntityID);
 }
 
-TEST(AnimationSystemTests, CurrentFrame_InvalidAnimationID_GivesDefaultAnimationFrame) {
+TEST(AnimationSystemTests, StartAnimation_CurrentFrame_IsFirstFrame) {
 	AnimationSystem<int> animation_system;
+	AnimationID animation_id = animation_system.add_animation(test_animation());
+	AnimationEntityID entity_id = AnimationEntityID(1);
 
-	int current_frame = animation_system.current_frame(INVALID_ANIMATION_ENTITY_ID);
+	std::expected<void, AnimationError> result = animation_system.start_animation(animation_id, entity_id, 0ms);
+	int current_frame = animation_system.current_frame(entity_id);
 
-	EXPECT_EQ(current_frame, 0);
+	EXPECT_TRUE(result.has_value());
+	EXPECT_EQ(current_frame, 111);
 }
-
-// start animation; get current frame => first frame
 
 // start animation; update into future; get current frame => second frame
