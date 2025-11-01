5a47016797a5f4b6e10a6ff47dc34f1140a236d2
Rasmus Källqvist <rkallqvist@microsoft.com>
2025-11-02 00:17:58 +0100

    Basic moving square in GameplayScene

--- src/game/scene/gameplay_scene.cpp
+++ src/game/scene/gameplay_scene.cpp
@@ -10,14 +10,39 @@
 
 namespace game {
 
+	void GameplayScene::initialize(engine::ResourceManager* resources, engine::CommandList* /*commands*/) {
+	}
+
 	void GameplayScene::update(const engine::Input& input, engine::CommandList* commands) {
 		if (input.keyboard.key_was_pressed_now(VK_ESCAPE)) {
 			commands->load_scene(MenuScene::NAME);
 		}
+
+		if (input.keyboard.key_is_pressed(VK_LEFT)) {
+			m_player_pos.x -= 1;
+		}
+		if (input.keyboard.key_is_pressed(VK_RIGHT)) {
+			m_player_pos.x += 1;
+		}
+		if (input.keyboard.key_is_pressed(VK_UP)) {
+			m_player_pos.y -= 1;
+		}
+		if (input.keyboard.key_is_pressed(VK_DOWN)) {
+			m_player_pos.y += 1;
+		}
 	}
 
 	void GameplayScene::draw(engine::Renderer* renderer) const {
-		renderer->draw_text(engine::DEFAULT_FONT_ID, 16, { 0, 0 }, engine::RGBA::white(), "Gameplay Scene");
+		engine::IVec2 world_origin = renderer->screen_resolution() / 2;
+
+		constexpr int player_size = 16;
+		engine::Rect player_rect = {
+			m_player_pos.x - player_size / 2,
+			m_player_pos.y - player_size / 2,
+			16,
+			16,
+		};
+		renderer->draw_rect_fill(player_rect + world_origin, engine::RGBA::green());
 	}
 
 } // namespace game
--- src/game/scene/gameplay_scene.h
+++ src/game/scene/gameplay_scene.h
@@ -2,13 +2,19 @@
 
 #include <engine/scene/scene.h>
 
+#include <engine/math/ivec2.h>
+
 namespace game {
 
 	class GameplayScene : public engine::Scene {
 	public:
 		static constexpr char NAME[] = "GameplayScene";
+		void initialize(engine::ResourceManager* resources, engine::CommandList* commands) override;
 		void update(const engine::Input& input, engine::CommandList* commands) override;
 		void draw(engine::Renderer* renderer) const override;
+
+	private:
+		engine::IVec2 m_player_pos;
 	};
 
 } // namespace game
