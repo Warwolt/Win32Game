78fb3598423283d64e014417f5653cd28bcdd865
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2025-11-09 04:50:55 +0100

    rename State to Application (#50)

--- src/application.cpp
+++ src/application.cpp
@@ -16,61 +16,61 @@ constexpr bool PROFILING_IS_ENABLED = true;
 constexpr bool PROFILING_IS_ENABLED = false;
 #endif
 
-struct State {
+struct Application {
 	engine::Engine engine;
 	game::GameData game;
 };
 
-static void pump_window_messages(State* state) {
+static void pump_window_messages(Application* app) {
 	CPUProfilingScope_Application();
 	MSG message;
 	while (PeekMessageA(&message, 0, 0, 0, PM_REMOVE)) {
 		TranslateMessage(&message);
 		DispatchMessageA(&message);
 		if (message.message == WM_QUIT) {
-			state->engine.should_quit = true;
+			app->engine.should_quit = true;
 		}
 	}
 }
 
-static void update_input(State* state) {
+static void update_input(Application* app) {
 	CPUProfilingScope_Application();
-	engine::update_input(&state->engine.input, state->engine.input_events, state->engine.window);
-	state->engine.input_events = {};
+	engine::update_input(&app->engine.input, app->engine.input_events, app->engine.window);
+	app->engine.input_events = {};
 
 	// HACK: we're not forwarding key events to DefWindowProc, so we have to handle ALT+F4 manually
-	const engine::Keyboard& keyboard = state->engine.input.keyboard;
+	const engine::Keyboard& keyboard = app->engine.input.keyboard;
 	if (keyboard.key_is_pressed(VK_MENU) && keyboard.key_was_pressed_now(VK_F4)) {
-		state->engine.should_quit = true;
+		app->engine.should_quit = true;
 	}
 }
 
-State* initialize_application(int argc, char** argv, HINSTANCE instance, WNDPROC on_window_event) {
-	State* state = new State {};
+Application* initialize_application(int argc, char** argv, HINSTANCE instance, WNDPROC on_window_event) {
+	Application* app = new Application {};
 	engine::CommandList init_commands;
 
 	/* Initialize game */
-	state->game = game::initialize(&init_commands);
+	app->game = game::initialize(&init_commands);
 
 	/* Initialize engine */
 	std::vector<std::string> args = std::vector<std::string>(argv, argv + argc);
-	std::optional<engine::Engine> engine = engine::initialize(args, instance, on_window_event, &state->game);
+	std::optional<engine::Engine> engine = engine::initialize(args, instance, on_window_event, &app->game);
 	if (!engine) {
 		MessageBoxA(0, "Failed to initialize engine", "Error", MB_OK | MB_ICONERROR);
 		exit(1);
 	}
-	state->engine = std::move(engine.value());
+	app->engine = std::move(engine.value());
 
 	/* Run initial commands */
-	init_commands.run_commands(&state->engine, state->engine.game_data);
+	init_commands.run_commands(&app->engine, app->engine.game_data);
 	LOG_INFO("Initialized");
 	LOG_INFO(PROFILING_IS_ENABLED ? "CPU profiling is enabled" : "CPU profiling is disabled");
 
-	return state;
+	return app;
 }
 
 LRESULT CALLBACK on_window_event(
-	State* state,
+	Application* app,
 	HWND window,
 	UINT message,
 	WPARAM w_param,
@@ -79,21 +79,21 @@ LRESULT CALLBACK on_window_event(
 	CPUProfilingScope_Application();
 	switch (message) {
 		case WM_SIZE: {
-			engine::IVec2 window_size = state->engine.window.on_resized();
+			engine::IVec2 window_size = app->engine.window.on_resized();
 		} break;
 
 		case WM_SYSCHAR:
 		case WM_SYSKEYDOWN:
 		case WM_KEYDOWN: {
 			uint32_t key_id = (uint32_t)w_param;
-			state->engine.input.keyboard.on_key_event(key_id, true);
+			app->engine.input.keyboard.on_key_event(key_id, true);
 			return (LRESULT)0;
 		} break;
 
 		case WM_SYSKEYUP:
 		case WM_KEYUP: {
 			uint32_t key_id = (uint32_t)w_param;
-			state->engine.input.keyboard.on_key_event(key_id, false);
+			app->engine.input.keyboard.on_key_event(key_id, false);
 			return (LRESULT)0;
 		} break;
 
@@ -108,67 +108,67 @@ LRESULT CALLBACK on_window_event(
 		} break;
 
 		case WM_MOUSEWHEEL: {
-			state->engine.input_events.mouse.mouse_wheel_delta += GET_WHEEL_DELTA_WPARAM(w_param) / WHEEL_DELTA;
+			app->engine.input_events.mouse.mouse_wheel_delta += GET_WHEEL_DELTA_WPARAM(w_param) / WHEEL_DELTA;
 		} break;
 
 		case WM_MOUSEMOVE: {
-			state->engine.input_events.mouse.mouse_x = (int16_t)GET_X_LPARAM(l_param);
-			state->engine.input_events.mouse.mouse_y = (int16_t)GET_Y_LPARAM(l_param);
+			app->engine.input_events.mouse.mouse_x = (int16_t)GET_X_LPARAM(l_param);
+			app->engine.input_events.mouse.mouse_y = (int16_t)GET_Y_LPARAM(l_param);
 		} break;
 
 		case WM_ACTIVATE: {
-			state->engine.window.on_focus_changed(LOWORD(w_param) != WA_INACTIVE);
+			app->engine.window.on_focus_changed(LOWORD(w_param) != WA_INACTIVE);
 		} break;
 
 		case WM_PAINT: {
 			/* Input */
-			update_input(state);
+			update_input(app);
 
 			/* Update */
 			engine::CommandList commands;
-			game::update(&state->game, state->engine.input, &commands);
-			engine::update(&state->engine, &commands);
+			game::update(&app->game, app->engine.input, &commands);
+			engine::update(&app->engine, &commands);
 
 			/* Render*/
-			game::draw(&state->engine.renderer, state->game);
-			engine::draw(&state->engine);
-			state->engine.renderer.render(&state->engine.resources);
-			state->engine.window.render_wm_paint(state->engine.renderer.bitmap());
+			game::draw(&app->engine.renderer, app->game);
+			engine::draw(&app->engine);
+			app->engine.renderer.render(&app->engine.resources);
+			app->engine.window.render_wm_paint(app->engine.renderer.bitmap());
 		} break;
 	}
 	return DefWindowProc(window, message, w_param, l_param);
 }
 
-bool update_application(State* state) {
-	state->engine.frame_timer.start();
+bool update_application(Application* app) {
+	app->engine.frame_timer.start();
 	{
 		/* Input */
-		pump_window_messages(state);
-		update_input(state);
+		pump_window_messages(app);
+		update_input(app);
 
 		/* Update */
 		engine::CommandList commands;
-		game::update(&state->game, state->engine.input, &commands);
-		engine::update(&state->engine, &commands);
+		game::update(&app->game, app->engine.input, &commands);
+		engine::update(&app->engine, &commands);
 
 		/* Draw */
-		game::draw(&state->engine.renderer, state->game);
-		engine::draw(&state->engine);
+		game::draw(&app->engine.renderer, app->game);
+		engine::draw(&app->engine);
 
 		/* Render */
-		state->engine.renderer.render(&state->engine.resources);
-		state->engine.window.render(state->engine.renderer.bitmap());
+		app->engine.renderer.render(&app->engine.resources);
+		app->engine.window.render(app->engine.renderer.bitmap());
 
 		/* Yield CPU to not stall OS */
 		Sleep(1);
 	}
-	state->engine.frame_timer.end();
+	app->engine.frame_timer.end();
 	CPUProfilingEndFrame();
 
-	return state->engine.should_quit;
+	return app->engine.should_quit;
 }
 
-void shutdown_application(State* state) {
+void shutdown_application(Application* app) {
 	LOG_INFO("Shutting down");
-	delete state;
+	delete app;
 }
