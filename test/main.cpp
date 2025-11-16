fa3f775d3b307ebbc8e9535b874665a92a838152
Rasmus Källqvist <rkallqvist@microsoft.com>
2025-11-16 18:18:39 +0100

    Lists

--- test/main.cpp
+++ test/main.cpp
@@ -23,12 +23,22 @@ constexpr char header_template[] = R"(
 <p style="margin-top: 0">{}</p>
 )";
 
-constexpr char report_stats_template[] = R"(
+constexpr char snapshot_stats_template[] = R"(
 <p>
-	✅ Passed snapshots: {}
-	<br />
-	❌ Failed snapshots: {}
+  ✅ Passed snapshots: {}
+  <br />
+  ❌ Failed snapshots: {}
 </p>
+)";
+
+constexpr char failed_snapshots_template[] = R"(
+<h2>Failed snapshots</h2>
+<ul>{}</ul>
+)";
+
+constexpr char all_snapshots_template[] = R"(
+<h2>All snapshots</h2>
+<ul>{}</ul>
 )";
 
 std::string report_header_html() {
@@ -37,17 +47,46 @@ std::string report_header_html() {
 	return std::format(header_template, timestamp);
 }
 
-std::string report_stats_html() {
+std::string snapshot_stats_html() {
 	const int passed_snapshots = 0;
 	const int failed_snapshots = 0;
-	return std::format(report_stats_template, passed_snapshots, failed_snapshots);
+	return std::format(snapshot_stats_template, passed_snapshots, failed_snapshots);
+}
+
+std::string failed_snapshots_html() {
+	const std::vector<std::string> failed_suite_names = {
+		"Test suite name 1",
+	};
+
+	std::string list_items;
+	for (const std::string& name : failed_suite_names) {
+		list_items += std::format("<li>{}</li>", name);
+	}
+
+	return std::format(failed_snapshots_template, list_items);
+}
+
+std::string all_snapshots_html() {
+	const std::vector<std::string> all_suite_names = {
+		"Test suite name 1",
+		"Test suite name 2",
+		"Test suite name 3",
+	};
+
+	std::string list_items;
+	for (const std::string& name : all_suite_names) {
+		list_items += std::format("<li>{}</li>", name);
+	}
+
+	return std::format(all_snapshots_template, list_items);
 }
 
 std::string snapshot_report_html() {
 	std::string html_body;
 	html_body += report_header_html();
-	html_body += report_stats_html();
-
+	html_body += snapshot_stats_html();
+	html_body += failed_snapshots_html();
+	html_body += all_snapshots_html();
 	return std::format(html_template, html_body);
 }
 
