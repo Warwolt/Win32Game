e7aa2791e69d87b37e1d7d007e31a166da51b571
Rasmus Källqvist <rkallqvist@microsoft.com>
2025-11-16 16:24:12 +0100

    start working on html snapshot report generation

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,34 @@
 #include <gtest/gtest.h>
 
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <format>
+
+constexpr char html_template[] =
+R"(<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Snapshot Test Report</title>
+  </head>
+  <body>
+	{}
+  </body>
+</html>)";
+
+
 int main(int argc, char** argv) {
+	/* Run tests */
 	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	int result = RUN_ALL_TESTS();
+
+	/* Produce snapshot report */
+	std::ofstream report_file;
+	std::filesystem::create_directory("snapshot_report");
+	report_file.open("snapshot_report/index.html");
+	report_file << std::format(html_template, "<p>Hello!</p>") << std::endl;
+	report_file.close();
+
+	return result;
 }
