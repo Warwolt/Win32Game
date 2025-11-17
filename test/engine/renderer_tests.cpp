d1f9ca81a692b037f9843bf6bcb5b4d54e8b65b8
Rasmus Källqvist <rkallqvist@microsoft.com>
2025-11-18 00:24:57 +0100

    Revert "check if test can fail in CI"
    This reverts commit 5420fde97c8ed31c7efa06a5b4fc8ccb34e142cc.

--- test/engine/renderer_tests.cpp
+++ test/engine/renderer_tests.cpp
@@ -11,7 +11,7 @@ TEST(RendererTests, ClearScreen_Turquoise) {
 	Renderer renderer = Renderer::with_bitmap(256, 240);
 	ResourceManager resources;
 
-	renderer.clear_screen({ 0, 0, 127, 255 });
+	renderer.clear_screen({ 0, 127, 127, 255 });
 	renderer.render(resources);
 
 	EXPECT_IMAGE_EQ_SNAPSHOT(renderer.bitmap().to_image());
